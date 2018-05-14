790ad86a2752336e4698ccca91ddb3e7d844758d
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-05-14 16:30:55 -0400

    Add connection config, simplified server.js, still no formatting.

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//Database Connection
+var mysql = require("mysql");
+
+//Import environmental variables from our variables.env file
+require("dotenv").config({ path: "variables.env" });
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
